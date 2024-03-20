9b5b9b9458596bcf61d5be053f595f7bc10b6f08
wu <yjrhgvbn@gmail.com>
2024-03-20 16:46:32 +0800

    init playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import Inspect from 'vite-plugin-inspect'
-import Unplugin from '../src/vite'
+import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
+import Unplugin from "../src/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    Inspect(),
-    Unplugin(),
-  ],
-})
+  plugins: [Inspect(), Unplugin(), react()],
+});
